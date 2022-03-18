d74b96cc7f1bfdf9cd545c255a1969d6f872ea47
Christopher <christopher.brandt09@gmail.com>
2022-03-18 16:31:11 +0100

    added more projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,8 +5,10 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import SlidingText from "../components/SlidingText";
 
+import memeImage from "../assets/images/projects/meme.webp";
 import sharringsImage from "../assets/images/projects/sharrings.webp";
 import cssBattleImage from "../assets/images/projects/cssbattle.webp";
+import dlduImage from "../assets/images/projects/dldu.webp";
 
 function Projects() {
 	const Logo = () => <h1 className="cursor-pointer text-4xl font-bold">cb</h1>;
@@ -44,18 +46,28 @@ function Projects() {
 			description: "A website to optimize css code for cssbattle.com",
 			href: "https://www.cssbattleoptimizer.com/",
 		},
+		{
+			title: "DLDU-Points",
+			image: dlduImage,
+			description: "A application for the DLDU event to display the current points in a OBS browser source",
+		},
+		{
+			image: memeImage,
+			empty: true,
+		},
 	];
 
 	return (
 		<>
 			<Navbar logo={<Logo />} links={links} />
-			<section className="w-full min-h-screen flex flex-col justify-center items-center relative p-20 mt-10">
+			<section className="w-full flex flex-col justify-center items-center relative p-6 md:p-12 mt-28 mb-12">
 				<div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
 					{projects &&
 						projects.map((project, i) => (
 							<ProjectCard
 								i={i}
 								key={i}
+								empty={project.empty}
 								href={project.href}
 								onClick={project.onClick}
 								title={project.title}
@@ -70,7 +82,7 @@ function Projects() {
 	);
 }
 
-const ProjectCard = ({ i, title, image, href, description, onClick = () => {} }) => {
+const ProjectCard = ({ i, empty, title, image, href, description, onClick = () => {} }) => {
 	return (
 		<motion.a
 			initial={{
@@ -88,12 +100,12 @@ const ProjectCard = ({ i, title, image, href, description, onClick = () => {} })
 			transition={{
 				delay: 0.1 * i,
 				ease: [0.6, 0.01, -0.05, 0.95],
-				duration: .6,
+				duration: 0.6,
 				type: "spring",
-				bounce: .3,
+				bounce: 0.3,
 			}}
 			whileHover={{
-				scale: 1.01
+				scale: 1.01,
 			}}
 			whileTap={{ scale: 0.99 }}
 			href={href}
@@ -102,11 +114,23 @@ const ProjectCard = ({ i, title, image, href, description, onClick = () => {} })
 			onClick={onClick}
 			className="flex flex-col rounded-md overflow-hidden bg-navbarBackgroundLight dark:bg-navbarBackgroundDark cursor-pointer shadow-smooth dark:shadow-none"
 		>
-			<img className="w-full rounded-b-2xl" src={image} />
-			<div className="w-full flex flex-col px-6 py-4">
-				<h1 className="text-3xl lg:text-4xl font-bold inline-block py-4 text-textDark dark:text-textLight">{title}</h1>
-				<p className="text-xl inline-block py-4 text-textDark dark:text-textLight">{description}</p>
-			</div>
+			{empty ? (
+				<div className="flex flex-col justify-between items-center h-full">
+					<img className="w-full rounded-b-2xl" src={memeImage}/>
+					<p className="text-3xl lg:text-4xl inline-block py-4 text-textDark dark:text-textLight">i need more projects...</p>
+				</div>
+			) : (
+				<>
+					{" "}
+					<img className="w-full rounded-b-2xl" src={image} />
+					<div className="w-full flex flex-col px-6 py-4">
+						<h1 className="text-3xl lg:text-4xl font-bold inline-block py-4 text-textDark dark:text-textLight">
+							{title}
+						</h1>
+						<p className="text-xl inline-block py-4 text-textDark dark:text-textLight">{description}</p>
+					</div>
+				</>
+			)}
 		</motion.a>
 	);
 };
